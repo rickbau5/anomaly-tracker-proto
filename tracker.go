6ea61302d014d5a60005a4f6001f064742a69eef
Rick Boss <rickeboss@gmail.com>
2018-01-06 16:35:08 -0800

    Use google.proto.Timestamp

--- tracker.go
+++ tracker.go
@@ -2,6 +2,6 @@ package anomalytracker
 
 // Publishable validates that all required fields are set for inserting.
 func (a *Anomaly) Publishable() bool {
-	return a != nil && a.Id != nil && a.System != nil &&
-		a.Type != nil && a.Name != nil
+	return a != nil && a.Id != "" && a.System != "" &&
+		a.Type != "" && a.Name != ""
 }
